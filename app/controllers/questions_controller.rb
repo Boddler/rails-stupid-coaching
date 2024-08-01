441fee596395082cbdd40671ff318d9fbd6c2854
Barry Roberts <barryjroberts@gmail.com>
2024-08-01 11:14:28 +0900

    updated css mainly

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,14 @@
 class QuestionsController < ApplicationController
-
   def ask
   end
 
   def answer
-    if params[:answer].downcase == "i am going to work!"
+    if params[:question].downcase == "i am going to work!"
       @answer = "Great!"
-    elsif params[:answer][-1] == "?"
+    elsif params[:question][-1] == "?"
       @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
     end
   end
-
 end
